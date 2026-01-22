22ccf3420e615bcd9b4f943c8c0eec3b53e0ffa2
Pedro Sandrini <sandrini.pedro@outlook.com>
2026-01-22 01:11:44 -0300

    chore(App): update SimpleFormBuilder usage with new API

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,26 @@ import {SimpleForm} from "./components/SimpleForm";
 import { SimpleFormBuilder } from './components/SimpleForm/SimpleFormBuilder';
 
 function App() {
+    const themes = [
+        {
+            id: "1",
+            value: "Cyber Security",
+        },
+        {
+            id: "2",
+            value: "Data Mining",
+        },
+        {
+            id: "3",
+            value: "DevSecOps",
+        }
+    ]
+
     const eventForm = new SimpleFormBuilder()
         .setTitle("Preencha para criar um novo evento:")
         .addTextField("name", "Nome do evento", "Summer Dev Show")
         .addDateField("date", "Data do evento")
+        .addSelect("theme", "Tema do Evento", "themes", themes)
         .setButtonLabel("Criar Evento")
         .build();
 
